eff4029afd36838fe7b0735f3b428bff56ddef96
Eastsky712 <eastsky712@gmail.com>
2025-10-14 10:36:37 +0900

    Dude, I'm actually so proud of this one. I got it without any help (except a techinical error). Holy this was so sick

--- BaekjoonPractice/Silver/2630.py
+++ BaekjoonPractice/Silver/2630.py
@@ -2,8 +2,11 @@ import math
 N = int(input())
 
 graph = [[] * N] * N
-check = [[0] * N] * N
+chk = [[2 for _ in range(N)] for _ in range(N)] 
 sizes = int(math.log2(N) + 1)
+totalBlue = 0
+totalWhite = 0
+
 
 for i in range(N):
     graph[i] = list(map(int,input().split(" ")))
@@ -29,12 +32,43 @@ def check(startX, startY, size, graph):
     else:
         return 2
 
+def used(startX, startY, size, fill):
+    #print("USED: " + str(startX) + " " + str(startY) + " " + str(size))
+    for x in range(size):
+        for y in range(size):
+            #print("X: " + str(startX + x) + " Y: " + str(startY + y))
+            curX = startX + x
+            curY = startY + y
+            chk[curX][curY] = fill
+    
+    
+
 for i in range(sizes):
-    for j in range((i)*4):
-        print(j)
+    curX = 0
+    curY = 0
+    for j in range(4 ** i):
+        curPaperWidth = N // (2 ** i)
+
+        #print(str(curX) + " " + str(curY) + " " + str(curPaperWidth))
+        if(chk[curX][curY] == 2):
+            result = check(curX, curY, curPaperWidth, graph)
+            if result == 0:
+                totalWhite += 1
+                used(curX, curY, curPaperWidth, 0)
+            elif result == 1:
+                totalBlue += 1
+                used(curX, curY, curPaperWidth, 1)
+        if (curX + curPaperWidth) % N == 0:
+            curX = 0
+            curY += curPaperWidth
+        else:
+            curX += curPaperWidth
         
 
-            
+        if curY >= N:
+            continue   
+     
+        
 
-
-print(graph)
+print(totalWhite)
+print(totalBlue)
