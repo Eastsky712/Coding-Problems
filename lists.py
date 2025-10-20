57e90a1ea57e0feaaea555e08a55ca7fb9db7d65
Eastsky712 <eastsky712@gmail.com>
2025-10-20 17:52:20 +0900

    Today, I started reading python crash course and basically strengthening my foundamentals.

---
+++ lists.py
@@ -0,0 +1,40 @@
+random_var = [1, 1.0, "1", '1']
+print(random_var)
+
+fruits = ["banana", "grapes", "apple", "watermelon"]
+print(f"My favorite fuirt growing up was {fruits[3].title()}")
+
+delete_test = ["stuff1", "stuff2", "stuff3"]
+del delete_test[1]
+print(delete_test)
+
+names = ["Charles", "Amy", "Hitchcock", "Terry", "Gina", "Jake"]
+names.insert(0, "Sculley")
+names.insert(4, "Rosa")
+names.append("Raymond")
+print(names)
+
+
+print("Original List Order:")
+print(names)
+print("Sorted List: ")
+print(sorted(names))
+print("Originial: ")
+print(names)
+
+
+locations = ["China", "Japan", "Thailand", "Singapore", "Malaysia"]
+print(locations)
+print(sorted(locations))
+print(locations)
+print(sorted(locations, reverse=True))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
+
